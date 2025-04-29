e2944d3df59dfd4d4fc839009e96a97dfb004647
pratiklohar <pratikwed@gmail.com>
2025-04-29 19:28:41 +0530

    changes.

--- src/main/java/com/myapp/springbootpostgresqlapp/service/impl/ProductServiceImpl.java
+++ src/main/java/com/myapp/springbootpostgresqlapp/service/impl/ProductServiceImpl.java
@@ -3,7 +3,7 @@ package com.myapp.springbootpostgresqlapp.service.impl;
 import com.myapp.springbootpostgresqlapp.constants.ErrorMessages;
 import com.myapp.springbootpostgresqlapp.dto.ProductDto;
 import com.myapp.springbootpostgresqlapp.exception.ResourceNotFoundException;
-import com.myapp.springbootpostgresqlapp.mapper.ProductMapper;
+import com.myapp.springbootpostgresqlapp.model.Product;
 import com.myapp.springbootpostgresqlapp.repository.ProductRepository;
 import com.myapp.springbootpostgresqlapp.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -15,22 +15,34 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
-    private final ProductMapper productMapper;
-
 
     @Override
     public List<ProductDto> getAllProducts() {
-        return productMapper.toDtoList(productRepository.findAll());
+        return productRepository.findAll()
+                .stream()
+                .map(this::toDto)
+                .toList();
     }
 
     @Override
     public ProductDto getProductById(Integer productId) {
-        return productMapper.toDto(productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException(ErrorMessages.PRODUCT_NOT_FOUND)));
+        return productRepository.findById(productId)
+                .map(this::toDto)
+                .orElseThrow(() -> new ResourceNotFoundException(ErrorMessages.PRODUCT_NOT_FOUND));
     }
 
     @Override
     public ProductDto addProduct(ProductDto productDto) {
-        return productMapper.toDto(productRepository.save(productMapper.toEntity(productDto)));
+        if(productRepository.existsByProductNameAndCategoryIdAndUnitWeightAndWeightType(
+                productDto.productName(),
+                productDto.categoryId(),
+                productDto.unitWeight(),
+                productDto.weightType()
+        )) {
+            throw new ResourceNotFoundException(ErrorMessages.PRODUCT_EXIST);
+        }else {
+            return toDto(productRepository.save(toEntity(productDto)));
+        }
     }
 
     @Override
@@ -39,6 +51,7 @@ public class ProductServiceImpl implements ProductService {
         productMapper.updateProductFromDto(productDto, product);
         return productMapper.toDto(productRepository.save(product));
     }
+
     @Override
     public void deleteProduct(Integer productId) {
         if (!productRepository.existsById(productId)) {
@@ -46,4 +59,34 @@ public class ProductServiceImpl implements ProductService {
         }
         productRepository.deleteById(productId);
     }
+
+// Convert the Product entity to Product DTO
+
+    private ProductDto toDto(Product product) {
+        return new ProductDto(
+                product.getProductId(),
+                product.getProductName(),
+                product.getCategoryId(),
+                product.getPrice(),
+                product.getQuantity(),
+                product.getUnitWeight(),
+                product.getWeightType(),
+                product.getPackagingType()
+        );
+    }
+
+    // Convert the Product DTO to Product entity
+    private Product toEntity(ProductDto productDto) {
+        return new Product(
+                productDto.productId(),
+                productDto.productName(),
+                productDto.categoryId(),
+                productDto.price(),
+                productDto.quantity(),
+                productDto.unitWeight(),
+                productDto.weightType(),
+                productDto.packagingType()
+        );
+    }
 }
+
